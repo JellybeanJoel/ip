b3db5d9693ff7272109161758b1fab8f569c11df
JellybeanJoel <e1398423@u.nus.edu>
2026-01-24 16:06:45 +0800

    Level 0 completed

---
+++ src/main/java/Jenie.java
@@ -0,0 +1,13 @@
+public class Jenie {
+    public static void main(String[] args) {
+        String greetings = 
+                  "  ______                  O      \n"
+                + " |__   _|  ___    _____   _    ___      \n"
+                + " _  | |   / __\\  |  _  | | |  / __\\    \n"
+                + "| |_| |  |  __/  | | | | | | |  __/     \n"
+                + "|_____/   \\___|  |_| |_| |_|  \\___|    \n";
+        String exit = "Goodbye. Hope to see you again soon! ";        
+        System.out.println("Hello I'm\n" + greetings);
+        System.out.println(exit);
+    }
+}
