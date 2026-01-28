1a4a64f4ba95fdee1bd0e8abb25fcca24899a384
JellybeanJoel <e1398423@u.nus.edu>
2026-01-28 10:24:39 +0800

    Level 1 completed

--- src/main/java/Jenie.java
+++ src/main/java/Jenie.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class Jenie {
     public static void main(String[] args) {
-        String greetings = 
-                  "  ______                  O      \n"
+        Scanner scanner = new Scanner(System.in);
+        String greetings = "  ______                  O      \n"
                 + " |__   _|  ___    _____   _    ___      \n"
                 + " _  | |   / __\\  |  _  | | |  / __\\    \n"
                 + "| |_| |  |  __/  | | | | | | |  __/     \n"
                 + "|_____/   \\___|  |_| |_| |_|  \\___|    \n";
-        String exit = "Goodbye. Hope to see you again soon! ";        
+
         System.out.println("Hello I'm\n" + greetings);
-        System.out.println(exit);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Goodbye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
+        scanner.close();
     }
 }
