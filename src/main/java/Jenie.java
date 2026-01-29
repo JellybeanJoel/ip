b1e49c620a4879482bcecafb2659917b5408db82
JellybeanJoel <e1398423@u.nus.edu>
2026-01-29 14:59:13 +0800

    Level 2 completed.

--- src/main/java/Jenie.java
+++ src/main/java/Jenie.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Jenie {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         String greetings = "  ______                  O      \n"
                 + " |__   _|  ___    _____   _    ___      \n"
                 + " _  | |   / __\\  |  _  | | |  / __\\    \n"
@@ -10,6 +13,7 @@ public class Jenie {
                 + "|_____/   \\___|  |_| |_| |_|  \\___|    \n";
 
         System.out.println("Hello I'm\n" + greetings);
+        System.out.println("What can I do for you?");
 
         while (true) {
             String input = scanner.nextLine();
@@ -17,7 +21,16 @@ public class Jenie {
                 System.out.println("Goodbye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(input);
+
+            if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+            }
         }
         scanner.close();
     }
